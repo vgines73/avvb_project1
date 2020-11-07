ebb5914073248379d52f83d4efc0d218a31a75e9
Victor Moscone <victormoscone@gmail.com>
2020-11-07 09:59:21 -0800

    Refactor: Improved and cleared up our initial fetch function.

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,6 +1,15 @@
-const proxyurl = "https://cors-anywhere.herokuapp.com/";
-const url = "https://superheroapi.com/api/1073994739725347/1/powerstats"; // site that doesn’t send Access-Control-*
-fetch(proxyurl + url) // https://cors-anywhere.herokuapp.com/https://example.com
-    .then(response => response.text())
-    .then(data => console.log(data))
-    .catch(() => console.log("Can’t access " + url + " response. Blocked by browser?"))
+let proxyurl = "https://cors-anywhere.herokuapp.com/";
+let url = "https://superheroapi.com/api/1073994739725347/1/powerstats";
+let requestUrl = proxyurl + url
+
+function getApi(requestUrl) {
+    fetch(requestUrl)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data);
+        })
+}
+
+getApi(requestUrl);
