203ab449330c5e74f8f715150066b166c3c9e633
Victor Moscone <victormoscone@gmail.com>
2020-11-07 13:02:09 -0800

    Fix: Readding lost Victor code. Fight_Script and fight.html

--- assets/scripts/fight_script.js
+++ assets/scripts/fight_script.js
@@ -1,9 +1,11 @@
 let proxyurl = "https://cors-anywhere.herokuapp.com/";
-let fightUrl = "https://superheroapi.com/api/1073994739725347/1/powerstats";
-let requestUrl = proxyurl + fightUrl
+let superUrl = "https://superheroapi.com/api/1073994739725347/1/powerstats";
+let fightUrl = proxyurl + superUrl;
+let chartUrl = "https://image-charts.com/chart";
+let barChart = document.querySelector("#barChart");
 
-function getApi(requestUrl) {
-    fetch(requestUrl)
+function getApi(chosenUrl) {
+    fetch(chosenUrl)
         .then(function (response) {
             return response.json();
         })
@@ -12,4 +14,14 @@ function getApi(requestUrl) {
         })
 }
 
-getApi(requestUrl);
+getApi(fightUrl);
+
+let popChart = document.createElement("img")
+popChart.setAttribute("src", "https://image-charts.com/chart?cht=bvg&chd=t:5,4,3,7,10&chxt=x&chs=500x300&chl=5|4|3|7|10&chxl=0:|Power|Speed|Intelligence|Durability|Will")
+barChart.append(popChart)
+// Chart type: cht=<type_code>
+// Chart data: chd=t:<data>,<data> etc. <-- Actually modifies the chart's visuals to match the numeric data.
+// Chart size: chs=<chart_size>x</chart_size>
+// Chart List: chl=<data>|<data>|<data> etc. <-- Adds the numeric ontop of its respective visual.
+// Chart XT: chxt=<X>,<Y> <-- both are optional.
+// Chart XL: chxl=0:|<parameter>|<parameter>| etc. <-- provides a title under the chart for the data.
