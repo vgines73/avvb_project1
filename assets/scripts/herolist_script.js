d4a48c1685dcf9722a03c67ba11b4b765167e944
“bonnielee” <bonlee8489@gmail.com>
2020-11-07 14:05:09 -0800

    EDIT: added new hero list JS

---
+++ assets/scripts/herolist_script.js
@@ -0,0 +1,10 @@
+let superheroList = [
+
+        [1, "A-Bomb"],
+        [2, Abe Sapien],
+        [3, Abin Sur],
+        [4, Abomination],
+        [5, Abraxas],
+        [6, Absorbing Man],
+        [7, Adam Monroe],
+        [
--- assets/scripts/index_script.js
+++ assets/scripts/index_script.js
@@ -13,3 +13,4 @@ function getApi(requestUrl) {
 }
 
 getApi(requestUrl);
+
